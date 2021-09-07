c33240fc142b3d44d1e8d552855b18b765397b1b
Georjane <pzgeorjane@gmail.com>
2021-09-07 15:04:06 +0100

    Generate user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_07_123343) do
+ActiveRecord::Schema.define(version: 2021_09_07_140319) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
