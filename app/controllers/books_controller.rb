8dde0407b3d6d7e83d7788ceb7a5d9dbbb6d1187
Georjane <pzgeorjane@gmail.com>
2021-09-07 14:01:15 +0100

    Add book controllers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,32 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:index, :show, :update, :destroy]
+
+  def index
+    @books = Book.all
+    json_response(@books)
+  end
+
+  def create
+    @book = Book.create!(book_params)
+    json_response(@book, :created)
+  end
+
+  def show
+    json_response(@book)
+  end
+
+  def destroy
+    @book.destroy
+    head :no_content
+  end
+
+  private
+
+  def book_params
+    params.permit(:title, :category)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
 end
